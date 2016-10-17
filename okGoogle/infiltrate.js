1d07bd2617dcb0506c509e3bb24dbe5279941835
jcpyun <johnchpyun@gmail.com>
2016-10-17 16:53:33 -0400

    infiltrate script works, but facebook security blocking protocol

--- okGoogle/infiltrate.js
+++ okGoogle/infiltrate.js
@@ -12,26 +12,36 @@ function onWindowLoad() {
 window.onload = onWindowLoad;
 
 
-
-
-chrome.runtime.onMessage.addListener(function(request, sender,sendResponse) {
-    message.innerText="wow";
-    if (request.action == "getSource") {
-        message.innerText = request.source;
-    }
-});
-setInterval(function(){ for (var i=0; i < document.getElementsByTagName('img').length; i++) {
-    document.getElementsByTagName('img')[i].src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/12924590_10209152047342870_7327219359152797257_n.jpg?oh=a31742d00731a5e7edbd3dd89ccb01de&oe=58A06EF6";
-  
-}; }, 1000);
-
-for (var i=0; i < document.getElementsByTagName('img').length; i++) {
-    document.getElementsByTagName('img')[i].src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/12924590_10209152047342870_7327219359152797257_n.jpg?oh=a31742d00731a5e7edbd3dd89ccb01de&oe=58A06EF6";
-}
-
 var myAudio = new Audio();        // create the audio object
 myAudio.src = chrome.extension.getURL('notSoFast.m4a'); // assign the audio file to it
-myAudio.play();                   // play the music
+
+if (document.location.href.includes("google")){
+  myAudio.play();
+  for (var i=0; i < document.getElementsByTagName('img').length; i++) {
+      document.getElementsByTagName('img')[i].src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/12924590_10209152047342870_7327219359152797257_n.jpg?oh=a31742d00731a5e7edbd3dd89ccb01de&oe=58A06EF6";
+      document.getElementsByTagName('img')[i].srcset="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/12924590_10209152047342870_7327219359152797257_n.jpg?oh=a31742d00731a5e7edbd3dd89ccb01de&oe=58A06EF6";
+  }
+}
+var switched= false;
+if (switched == false && document.location.href.includes("google")){
+  document.body.innerHTML = document.body.innerHTML.replace('google', 'Ben Zhang');
+  document.body.innerHTML = document.body.innerHTML.replace('Google', 'Ben Zhang');
+  document.body.innerHTML = document.body.innerHTML.replace(/google/g, 'Ben Zhang');
+  document.body.innerHTML = document.body.innerHTML.replace(/Google/g, 'Ben Zhang');
+  switched = true;
+}
+if (switched == false ){
+  setInterval(function(){ document.body.innerHTML = document.body.innerHTML.replace('Ben Zhang', 'Google');
+  document.body.innerHTML = document.body.innerHTML.replace('ben zhang', 'Google');
+  document.body.innerHTML = document.body.innerHTML.replace(/Ben Zhang/g, 'Google');
+  document.body.innerHTML = document.body.innerHTML.replace(/ben zhang/g, 'Google');
+  switched = true; }, 2000);
+  
+}
+
+
+
+                  // play the music
 // document.body.style.backgroundColor="#88b719";
 
 
