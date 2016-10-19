99ab4cf17e31aabc22f27a1082b9bf26fb75e760
jcpyun <johnchpyun@gmail.com>
2016-10-19 17:02:33 -0400

    bloomberg integration complete. Will integrate d3js possibly

--- okGoogle/bloomberg.js
+++ okGoogle/bloomberg.js
@@ -1,1 +1,19 @@
-http://www.bloomberg.com/markets/chart/data/1D/TSLA:US
+// http://www.bloomberg.com/markets/chart/data/1D/TSLA:US
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = process;
+xhr.open("GET", "http://www.bloomberg.com/markets/chart/data/1D/TSLA:US", true);
+xhr.send();
+var output;
+
+function process()
+{
+  if (xhr.readyState == 4) {
+    var resp = JSON.parse(xhr.responseText);
+
+    // resp now has the text and you can process it.
+    output= resp.data_values[resp.data_values.length-1];
+    output= output[1];
+    document.getElementById('price').innerHTML=output;
+  }
+}
