2d7018387c09003f28906f7c98695ed1aa3c1211
jcpyun <johnchpyun@gmail.com>
2016-10-19 17:48:40 -0400

    company infiltration needs fixing. fix bloomberg.js

--- okGoogle/bloomberg.js
+++ okGoogle/bloomberg.js
@@ -1,11 +1,20 @@
 // http://www.bloomberg.com/markets/chart/data/1D/TSLA:US
 
+var companies=["TSLA","AAPL","MSFT","AMZN","EBAY"]
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = process;
-xhr.open("GET", "http://www.bloomberg.com/markets/chart/data/1D/TSLA:US", true);
-xhr.send();
-var output;
 
+for (var i; i ++; i < companies.length){
+    var url="http://www.bloomberg.com/markets/chart/data/1D/company:US"
+    url.replace("companies",companies[i])
+    xhr.open("GET", url, true);
+    xhr.send();
+}
+
+var output;
+var url="http://www.bloomberg.com/markets/chart/data/1D/company:US"
+xhr.open("GET", url, true);
+xhr.send();
 function process()
 {
   if (xhr.readyState == 4) {
@@ -17,3 +26,23 @@ function process()
     document.getElementById('price').innerHTML=output;
   }
 }
+
+// http://www.bloomberg.com/markets/chart/data/1D/TSLA:US
+
+// var xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = process;
+// xhr.open("GET", "http://www.bloomberg.com/markets/chart/data/1D/TSLA:US", true);
+// xhr.send();
+// var output;
+
+// function process()
+// {
+//   if (xhr.readyState == 4) {
+//     var resp = JSON.parse(xhr.responseText);
+
+//     // resp now has the text and you can process it.
+//     output= resp.data_values[resp.data_values.length-1];
+//     output= output[1];
+//     document.getElementById('price').innerHTML=output;
+//   }
+// }
